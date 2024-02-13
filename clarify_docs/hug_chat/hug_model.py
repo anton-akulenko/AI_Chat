4cc4379f8fa77eeddf7f954bac9249633b6f2f36
anton-akulenko <anton.htl@gmail.com>
2024-02-13 16:02:09 +0200

    hug chat done

--- clarify_docs/hug_chat/hug_model.py
+++ clarify_docs/hug_chat/hug_model.py
@@ -12,14 +12,24 @@ cookies = sign.login()
 sign.saveCookiesToDir()
 
 chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
-
+chatbot.share_conversation()
 
 def create_conversation() -> str:
     id_ = chatbot.new_conversation()
+    chatbot.change_conversation(id_)
     return id_
+#
+#
+# def get_answer(conversation_id: str, question: str) -> str:
+#     chatbot.change_conversation(conversation_id)
+#     answer = chatbot.chat(question)
+#     return answer
 
-
-def get_answer(conversation_id: str, question: str) -> str:
-    chatbot.change_conversation(conversation_id)
+def get_answer(question: str) -> str:
     answer = chatbot.chat(question)
     return answer
+
+def get_list():
+    info = chatbot.get_conversation_info()
+    # print(info.id, info.title, info.model, info.system_prompt, info.history)
+    return [info.id, info.title, info.model]
--- clarify_docs/hug_chat/views.py
+++ clarify_docs/hug_chat/views.py
@@ -4,19 +4,34 @@ from django.shortcuts import redirect
 
 from llm_chat.models import UserData
 from .models import HugChatMessage
-from .hug_model import create_conversation, get_answer
+from .hug_model import get_answer, get_list, create_conversation
 
 
 @login_required
 def hug_chat(request):
-    user = UserData.objects.filter(id=request.user.id).get()
+    user_data = request.user
+    try:
+        user = UserData.objects.get(user=user_data)
+    except UserData.DoesNotExist:
+        user = UserData.objects.create(user=user_data, total_files_uploaded=0, total_questions_asked=0)
+
+    # user = UserData.objects.filter(id=request.user.id).get()
     requests = [request_ for request_ in HugChatMessage.objects.filter(user_id=user.id).all()]
 
+    message_list = get_list()
+
     if request.method == "POST":
-        message = request.POST["text"]
-        answer = get_answer(create_conversation(), message)
+        if 'create-new-chat' in request.POST:
+            create_conversation()
+            message = 'Hello!'
+            answer = get_answer(message)
+        else:
+            message = request.POST["text"]
+            answer = get_answer(message)
         request_ = HugChatMessage(user_id=user.id, message=message, answer=answer)
         requests.append(request_)
         request_.save()
 
-    return render(request, 'hug_chat/chat_hug.html', {'requests': requests})
+    return render(request, 'hug_chat/chat_hug.html', {'requests': requests, "msg_list": message_list})
+
+
