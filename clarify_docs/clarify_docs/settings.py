2b6d0f232de449bc9f4b59910daf6a57831ac5ad
anton-akulenko <anton.htl@gmail.com>
2024-02-13 16:40:48 +0200

    models for local

--- clarify_docs/clarify_docs/settings.py
+++ clarify_docs/clarify_docs/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "profiles",
+    "chats",
     "hug_chat",
     "llm_chat",
     'users',
